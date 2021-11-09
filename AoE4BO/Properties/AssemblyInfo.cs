12cc0a023fcaad752588260386d021a829add23b
Moritz Maart <m.maart@gmx.de>
2021-11-09 19:35:07 +0100

    Little bugfix

---
+++ AoE4BO/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("AoE4BO")]
+[assembly: AssemblyDescription("Overlay for Age of Empires 4 to lern build orders")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("m.maart@gmx.de")]
+[assembly: AssemblyProduct("AoE4BO.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("c321fc65-6f01-48c6-a1a2-d034440ab344")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("0.9.3.0")]
+[assembly: AssemblyFileVersion("0.9.3.0")]
+[assembly: NeutralResourcesLanguage("en")]
